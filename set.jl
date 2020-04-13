ef4dc87f492bc5f683669906eb4400be057eab5c
Sebastian Steiner <sebastian.steiner@tuta.io>
2020-04-13 13:39:26 -0400

    added random functions

--- set.jl
+++ set.jl
@@ -1,3 +1,6 @@
+using Random
+using Statistics
+
 function except(arr, i)
     return [arr[1:(i - 1)]; arr[(i + 1):end]]
 end
@@ -45,27 +48,43 @@ function gen_cards()
     return cards
 end
 
-function check_game(table::Array{NTuple{4,Int},1}, reserve::Array{NTuple{4,Int},1})::Bool
+function card_to_str(card)
+    return string(card[1], card[2], card[3], card[4])
+end
+
+function check_game(table::Array{NTuple{4,Int},1}, reserve::Array{NTuple{4,Int},1}, print::Bool)::Bool
     if length(table) == 0
         return true
     end
     if length(reserve) == 0
         sets = find_sets(table)
         if length(sets) == 0
+            if print
+                println("No more sets: ", table)
+            end
             return false
         end
         for set in sets
-            if (!check_game(except_inds(table, set[1], set[2], set[3]), reserve[4:end]))
+            if (!check_game(except_inds(table, set[1], set[2], set[3]), reserve[4:end], print))
+                if print
+                    println(card_to_str(table[set[1]]), ",", card_to_str(table[set[2]]), ",", card_to_str(table[set[3]]))
+                end
                 return false
             end
         end
     else
         sets = find_sets(table)
         if length(sets) == 0
+            if print
+                println("No more sets: ", table)
+            end
             return false
         end
         for set in sets
-            if (!check_game([except_inds(table, set[1], set[2], set[3]);reserve[1:3]], reserve[4:end]))
+            if (!check_game([except_inds(table, set[1], set[2], set[3]);reserve[1:3]], reserve[4:end], print))
+                if print
+                    println(card_to_str(table[set[1]]), ",", card_to_str(table[set[2]]), ",", card_to_str(table[set[3]]))
+                end
                 return false
             end
         end
@@ -73,15 +92,65 @@ function check_game(table::Array{NTuple{4,Int},1}, reserve::Array{NTuple{4,Int},
     return true
 end
 
-function main()
+function check(i)
     all = gen_cards()
-    table = all[59:69]
-    reserve = all[70:81]
-    check_game(table, reserve) # liefert false
-    #cards = [(0,0,0,0),(0,0,0,1),(0,0,1,2)]
-    #check_game(cards,Array{NTuple{4,Int},1}(undef,0))
-    #cards = gen_cards()
-    #println(check_game(cards[70:81],Array{NTuple{4,Int},1}(undef, 0)))
+    table = all[i:11+i]
+    reserve = all[i+12:end]
+    return check_game(table, reserve, true)
+end
+
+function to_card(str)
+    return (parse(Int, str[1]), parse(Int, str[2]), parse(Int, str[3]), parse(Int, str[4]))
+end
+
+function to_cards(str)
+    strs = split(rstrip(str), "\n")
+    strs = filter!(e->e≠"",strs)
+    cards = Array{NTuple{4,Int},1}(undef,length(strs))
+    for i in 1:length(strs)
+        cards[i] = to_card(strs[i])
+    end
+    return cards
+end
+
+function find_dups(arr)
+    for i in 1:length(arr)
+        for j in i+1:length(arr)
+            if arr[i] == arr[j]
+                println("Duplicates at: ", i, ",", j)
+                println("\tDuplicate: ", arr[i])
+                return false
+            end
+        end
+    end
+    return true
+end
+
+function check_str(str)
+    cards = to_cards(str)
+    if !find_dups(cards)
+        return false
+    end
+    if length(cards) >= 12
+        check_game(cards[1:12], cards[13:end], true)
+    else
+        check_game(cards[1:end], Array{NTuple{4,Int},1}(undef, 0), true)
+    end
+end
+
+function rng_check()
+    all = gen_cards()
+    while true
+        cards = shuffle(all)
+        if check_game(cards[1:12], cards[13:end], false)
+            println(cards)
+            return
+        end
+    end
+end
+
+function main()
+    
 end
 
 main()
